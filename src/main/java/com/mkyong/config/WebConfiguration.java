e63c95d42c27216eec74fbc260f75b9bdfb3df22
Marius Neculoiu <neculoiu.marius@gmail.com>
2017-05-16 20:11:45 +0300

    First commit

---
+++ src/main/java/com/mkyong/config/WebConfiguration.java
@@ -0,0 +1,19 @@
+package com.mkyong.config;
+
+import org.h2.server.web.WebServlet;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by marius on 5/16/17.
+ */
+@Configuration
+public class WebConfiguration {
+    @Bean
+    ServletRegistrationBean h2servletRegistration(){
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean( new WebServlet());
+        registrationBean.addUrlMappings("/console/*");
+        return registrationBean;
+    }
+}
